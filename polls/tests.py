21305955fc9b0621308fbba995f1a8cadb6c353c
Bob-dj <hnzj13@hotmail.com>
2015-08-09 23:01:52 +0800

    test

--- polls/tests.py
+++ polls/tests.py
@@ -4,6 +4,17 @@ import datetime
 from django.utils import timezone
 from django.test import TestCase
 from .models import  Question
+from django.core.urlresolvers import reverse
+
+def create_question(question_text, days):
+    """
+    Creates a question with the given `question_text` published the given
+    number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text,
+                                   pub_date=time)
 
 class QuesitonMethodTests(TestCase):
 
@@ -36,15 +47,7 @@ class QuesitonMethodTests(TestCase):
         recent_question = Question(pub_date=time)
         self.assertEqual(recent_question.was_published_recently(), True)
 
-def create_question(question_text, days):
-    """
-    Creates a question with the given `question_text` published the given
-    number of `days` offset to now (negative for questions published
-    in the past, positive for questions that have yet to be published).
-    """
-    time = timezone.now() + datetime.timedelta(days=days)
-    return Question.objects.create(question_text=question_text,
-                                   pub_date=time)
+
 
 
 class QuestionViewTests(TestCase):
@@ -104,6 +107,7 @@ class QuestionViewTests(TestCase):
             response.context['latest_question_list'],
             ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
+
 
 class QuestionIndexDetailTests(TestCase):
     def test_detail_view_with_a_future_question(self):
